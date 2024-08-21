009b4bfb6f44d4af37cca4ff27a5b3b9a082c0b0
zaidbinnaveed <142867727+zaidbinnaveed@users.noreply.github.com>
2024-08-21 09:43:48 +0500

    Create Task 01.py

---
+++ Lab 01/Task 01.py
@@ -0,0 +1,6 @@
+weight=float(input('Enter the value for Weight: '))
+height=float(input('Enter the value for Height: '))
+
+BMI = weight / (height*height)
+
+print("The BMI (Body Mass Index) of these values is :" , BMI)
