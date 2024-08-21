2b766e200f2fc8e458dbdefa872e99f032706c5f
zaidbinnaveed <142867727+zaidbinnaveed@users.noreply.github.com>
2024-08-21 10:01:08 +0500

    Create Task 04.py

---
+++ Lab 01/Task 04.py
@@ -0,0 +1,7 @@
+array=[]
+sum=0
+for y in range(5):
+    user_input=int(input('Enter number: '))
+    array.append(user_input)
+    sum=sum+user_input
+print('Sum of all the given numbers is :',sum)
