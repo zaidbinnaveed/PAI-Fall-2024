13934f2d0664ca2f284f56d22d4f7625f94e0e9d
zaidbinnaveed <142867727+zaidbinnaveed@users.noreply.github.com>
2024-08-28 14:02:42 +0500

    Create Task 02.py

---
+++ Lab 02/Task 02.py
@@ -0,0 +1,14 @@
+word = (str(input("Enter a word: ")))
+size=len(word)
+vowels = {'a','e','i','o','u','A','E','I','O','U'}
+if(size>0):
+    last_letter=word[-1]
+    if(last_letter in vowels):
+        print(f"The last letter {last_letter} is a vowel")
+    elif(last_letter not in vowels):
+        print(f"The last letter {last_letter} is a consonant")
+    else:
+        print(f"The last letter {last_letter} is neither a vowel nor a consonent")
+        
+    
+    
